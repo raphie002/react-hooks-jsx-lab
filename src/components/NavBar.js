20c2e57da60a802248e4a5310ceb0b703f5539ed
Raphael <raphael.njoroge02@gmail.com>
2025-07-13 15:45:03 +0300

    first commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,14 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+  <nav>
+    <ul>
+      <li><a href="#home">Home</a></li>
+      <li><a href="#about">About</a></li>
+    </ul>
+  </nav>
+  )
 }
 
 export default NavBar;
